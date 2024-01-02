edde667b22ab8da13b457a1db30b71f74590c00d
Ximo <ximo.dev.94@gmail.com>
2024-01-02 10:18:59 +0100

    feat: add missing player logic

--- player.py
+++ player.py
@@ -15,8 +15,19 @@ class Player():
     def __len__(self) -> int:
         return len(self.cards)
     
-    def draw_card(self) -> Card:
-        return self.cards.pop()
+    def draw_card(self) -> Card | None:
+        if len(self) > 0:
+            return self.cards.pop()
+    
+    def draw_cards(self, amount: int) -> list[Card]:
+        if len(self) < amount:
+            return self.cards
+        
+        cards = []
+        for _ in range(0, amount):
+            cards.append(self.cards.pop())
+        
+        return cards
     
     def take_cards(self, new_cards: list[Card]) -> None:
         for card in new_cards:
